dd208695c60259058233a989a0dd3fbd9b210ea8
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-11-11 14:27:31 +0100

    header started

--- src/App.vue
+++ src/App.vue
@@ -24,10 +24,9 @@ export default {
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+  /*   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2c3e50; */
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,81 @@
 <template>
   <div>
-    <h1 class="">ciao</h1>
+    <div class="my_header">
+      <img src="../img/dc-logo.png" alt="" />
+      <nav class="my_navbar">
+        <ul>
+          <li v-for="(item, i) in navBarItems" :key="i">
+            <a href="item.href">{{ item.text }}</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      navBarItems: [
+        {
+          text: "CHARACTERS",
+          href: "#",
+        },
+        {
+          text: "COMICS",
+          href: "#",
+        },
+        {
+          text: "MOVIES",
+          href: "#",
+        },
+        {
+          text: "TV",
+          href: "#",
+        },
+        {
+          text: "GAMES",
+          href: "#",
+        },
+        {
+          text: "COLLECTIBLES",
+          href: "#",
+        },
+        {
+          text: "VIDEOS",
+          href: "#",
+        },
+        {
+          text: "FANS",
+          href: "#",
+        },
+        {
+          text: "NEWS",
+          href: "#",
+        },
+        {
+          text: "SHOP",
+          href: "#",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-h1 {
-  color: red;
+.my_header {
+  background-color: silver;
+}
+.my_navbar {
+  display: inline-block;
+}
+.my_navbar ul li {
+  display: inline-block;
+  padding: 10px;
 }
 </style>
 
