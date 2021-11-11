93548bf0df578960033b71e60fa647882e5afb3c
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-11-11 16:07:47 +0100

    update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -26,7 +26,31 @@
         </ul>
       </div>
     </div>
-    <div class="menu_section">aa</div>
+    <div class="menu_section">
+      <div class="my_cont_footer layout_menu_footer">
+        <div class="my_col_4">
+          <ul>
+            <li>asdasd</li>
+            <li>asdasd</li>
+            <li>asdasd</li>
+            <li>asdasd</li>
+          </ul>
+          <ul>
+            <li>asdasd</li>
+            <li>asdasd</li>
+            <li>asdasd</li>
+            <li>asdasd</li>
+          </ul>
+          <ul>
+            <li>asdasd</li>
+            <li>asdasd</li>
+            <li>asdasd</li>
+            <li>asdasd</li>
+          </ul>
+        </div>
+        <div class="dc_logo_bg my_col_8"></div>
+      </div>
+    </div>
     <div class="social_section">aa</div>
   </div>
 </template>
@@ -42,11 +66,12 @@ export default {
 .my_footer {
   background-color: blanchedalmond;
 }
+/* BLUE SECTION */
 .my_cont_footer {
   margin: auto;
   max-width: 1140px;
 }
-.my_cont_footer ul {
+.blue_section .my_cont_footer ul {
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -58,14 +83,40 @@ export default {
 }
 .blue_section img {
   width: 40px;
+  vertical-align: middle;
 }
 .blue_section ul li {
   display: inline-block;
   padding: 10px;
 }
 .blue_section ul li a {
-  color: black;
+  color: rgb(255, 255, 255);
   font-size: 13px;
   text-decoration: none;
+  padding-left: 10px;
+}
+/* ************ */
+
+.menu_section {
+  background-image: url(../img/footer-bg.jpg);
+  object-fit: cover;
+}
+.layout_menu_footer {
+  display: flex;
+}
+
+.dc_logo_bg {
+  background-image: url(../img/dc-logo-bg.png);
+  background-position: center;
+  background-repeat: no-repeat;
+}
+.my_col_4 {
+  width: calc((100% / 12) * 4);
+}
+.my_col_8 {
+  width: calc((100% / 12) * 8);
+}
+.my_col_12 {
+  width: calc((100% / 12) * 12);
 }
 </style>
